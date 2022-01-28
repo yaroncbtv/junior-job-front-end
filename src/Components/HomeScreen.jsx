5141b58eee4d774d43cc3d59a15d94989d95f9f6
Yaron Cohen <yaroncbtv@gmail.com>
2022-01-28 21:49:12 +0200

    fix btn home page

--- src/Components/HomeScreen.jsx
+++ src/Components/HomeScreen.jsx
@@ -185,14 +185,14 @@ export default function HomeScreen() {
               {
                 login ? 
                 <div className="mt-5 sm:mt-8 sm:flex sm:justify-center lg:justify-start">
-                <button onClick={getStarted} className="mt-3 sm:mt-0 sm:ml-3">
-                  <div
+                <div onClick={getStarted} className="mt-3 sm:mt-0 sm:ml-3">
+                  <button
                     
                     className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 md:py-4 md:text-lg md:px-10"
                   >
                     Get started
-                  </div>
-                </button>
+                  </button>
+                </div>
                 <div className="mt-3 sm:mt-0 sm:ml-3">
                   <button
                     
